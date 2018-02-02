5f3c2248989cd02df45ca21cb9bb13cbc491bb32
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-02-02 03:15:19 +0100

    v0.1.1
    HTML
    + Added button that can display auto-scroll config window
    + Added sctructure of auto-scroll config window
    JAVASCRIPT
    + Added functions that can handle all events for config window's buttons
    + Added code that can detect actual configuration
    + Added function that can scroll page
    CSS
    + Modified old style
    + Added style for auto-scroll config window

--- scroll.js
+++ scroll.js
@@ -10,6 +10,9 @@ $(document).ready(function(){
     
     //main
     bttnActions();
+    cfgBttn();
+    drctBttn();
+    scrollBttn();
     
     $(window).scroll(function(){
         checkPos();
@@ -56,5 +59,90 @@ $(document).ready(function(){
         if(usrPos>pageHeight-distance-windowHeight){
             $(".bckBottom").fadeOut(btnDuration);
         }
+    }
+    
+    function cfgBttn(){
+        
+        var cfgBttn = document.getElementsByClassName("autoBtn")[0];
+        
+        cfgBttn.addEventListener("click", function(){
+            var isHere = $(".cfg").css("display");
+
+            switch(isHere){
+                case "none":
+                    $(".cfg").fadeIn(btnDuration);
+                    break;
+                case "block":
+                    $(".cfg").fadeOut(btnDuration);
+                    break;
+                default:
+                    console.log("Default in switch");
+            }
+            
+        });
+    }
+    
+    function scrollBttn(){
+        
+        
+        
+        $(".scrollStrt").click(function(){
+            if($("#activeBttn").hasClass("scrollTop") == true){
+                var drct = "top";
+            }
+            if($("#activeBttn").hasClass("scrollBottom") == true){
+                var drct = "bottom";
+            }
+            letsScroll(true, drct);
+        });
+        $(".scrollStp").click(function(){
+            var drct = undefined;
+            letsScroll(false, drct);
+        });
+    }
+    
+    function letsScroll(state, direction){
+        
+        switch(state){
+            case true:
+                switch(direction){
+                    case "top":
+                        var where = 0;
+                        break;
+                    case "bottom":
+                        var where = $("html").height();
+                        break;
+                    default:
+                        console.log("Default in switch");
+                }
+                var yourPos = $(window).scrollTop();
+                
+                var dst = Math.abs(yourPos - where);
+                var spd = $(".scrollRng").val();
+                var tm = dst/spd;
+                console.log(tm);
+                console.log("#"+yourPos+" # "+where);
+                
+                $("html").animate({
+                    scrollTop: where
+                },1000*tm,"linear");
+                break;
+            case false:
+                $("html").stop();
+                break;
+            default:
+                console.log("Default in switch");
+        }
+    }
+    
+    function drctBttn(){
+        $(".scrollTop").click(function(){
+            $(this).attr("id","activeBttn");
+            $(".scrollBottom").attr("id","");
+        });
+        $(".scrollBottom").click(function(){
+            $(this).attr("id","activeBttn");
+            $(".scrollTop").attr("id","");
+        });
     }
 });
